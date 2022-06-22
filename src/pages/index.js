122e6c32cd7948859cc30c08b4618d506c75a176
Yannik Hangst <yanso1@posteo.de>
2022-06-22 12:48:16 +0200

    add header to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ export default function HomePage() {
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Helmet>
-			<h1>Home</h1>
 		</Layout>
 	);
 }
