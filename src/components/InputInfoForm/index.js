ab99b31db239dd8c16d5abb03f2a4c25b020ab87
Yannik Hangst <yanso1@posteo.de>
2022-06-22 15:17:50 +0200

    add state and changeName function

--- src/components/InputInfoForm/index.js
+++ src/components/InputInfoForm/index.js
@@ -1,7 +1,16 @@
+import {useState} from 'react';
+
 export default function InputInfoForm() {
+	const [enteredName, setEnteredName] = useState('');
+
+	const nameChangeHandler = event => {
+		setEnteredName(event.target.value);
+	};
+
 	const handleSubmit = event => {
 		event.preventDefault();
 		alert('submit!');
+		setEnteredName('');
 	};
 
 	return (
@@ -10,7 +19,7 @@ export default function InputInfoForm() {
 				<section>
 					<label>
 						Input Name:
-						<input type="text"></input>
+						<input type="text" value={enteredName} onChange={nameChangeHandler}></input>
 					</label>
 				</section>
 				<section>
