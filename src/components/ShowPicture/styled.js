8f141cd20540551bc476694c74faa0daabded128
Yannik Hangst <yanso1@posteo.de>
2022-06-22 12:22:47 +0200

    add basic styling

---
+++ src/components/ShowPicture/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import {pxToRem} from '../../utils/unit';
+
+const StyledPicture = styled.img`
+	display: flex;
+	justify-content: center;
+	width: 40vw;
+	height: 20vh;
+	margin: 2em auto;
+	border-radius: 50%;
+`;
+
+export default StyledPicture;
