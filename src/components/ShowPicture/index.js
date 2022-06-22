ce4303f910cd8189f21ada2bb472b92e56b29679
Yannik Hangst <yanso1@posteo.de>
2022-06-22 11:41:59 +0200

    fix img

--- src/components/ShowPicture/index.js
+++ src/components/ShowPicture/index.js
@@ -1,5 +1,8 @@
 export default function ShowPicture() {
 	<>
-		<img src="https://unsplash.com/photos/N04FIfHhv_k" alt="mock_img"></img>
+		<img
+			src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+			alt="mock_img"
+		></img>
 	</>;
 }
