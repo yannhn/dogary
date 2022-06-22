495b4f6bc4a4360a57584756d0c8a81594629ae4
Yannik Hangst <yanso1@posteo.de>
2022-06-22 16:16:50 +0200

    add card content

---
+++ src/components/AddFoodCard/index.js
@@ -0,0 +1,12 @@
+export default function AddFoodCard() {
+	return (
+		<>
+			<button>Add new food</button>
+			<h2>Food</h2>
+			<h3>Type of food</h3>
+			<p>What: DOGNAME ate 80gram</p>
+			<p>When: At 7:30</p>
+			<p>Current food / goal</p>
+		</>
+	);
+}
