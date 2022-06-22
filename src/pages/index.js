16bf3c4f39a2f43956701f83b30462cacc1c3148
Yannik Hangst <yanso1@posteo.de>
2022-06-22 16:02:26 +0200

    connect input information to button-click

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
+import {useState} from 'react';
 import {Helmet} from 'react-helmet';
 
 import InputInfoForm from '../components/InputInfoForm';
 import Layout from '../components/Layout';
 
 export default function HomePage() {
+	const [showInputInfo, setShowInputInfo] = useState(false);
+
+	const handleInfo = () => {
+		setShowInputInfo(!showInputInfo);
+		console.log(showInputInfo);
+	};
+
 	return (
 		<Layout>
 			<Helmet>
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Helmet>
-			<button>InputInfoForm</button>
-			<InputInfoForm></InputInfoForm>
+			<section>
+				<button onClick={handleInfo}>
+					{showInputInfo ? 'hide InputForm' : 'show InputForm'}
+				</button>
+			</section>
+			{showInputInfo && <InputInfoForm />}
 		</Layout>
 	);
 }
