b49cac7ea0c35743208517b89adcdd4323e88e5c
Yannik Hangst <yanso1@posteo.de>
2022-06-23 11:03:06 +0200

    apply basic styling to form

--- src/components/InputFood/index.js
+++ src/components/InputFood/index.js
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 
+import {InputFoodContainer} from './styled';
+
 export default function InputFood() {
 	const [showFormContainer, setShowFormContainer] = useState(false);
 	const handleCloseForm = () => {
@@ -7,23 +9,27 @@ export default function InputFood() {
 	};
 	return (
 		<>
-			<form>
-				<h4>Your dog</h4>
-				<section>
-					<label>
-						How many gram did the dog eat
-						<input type="text"></input>
-					</label>
-				</section>
-				<section>
-					<label>
-						When did the dog eat
-						<input type="time" defaultValue="08:00"></input>
-					</label>
-				</section>
-				<button onClick={handleCloseForm}>Cancel</button>
-				<button>Add</button>
-			</form>
+			<InputFoodContainer>
+				<form>
+					<h4>Your dog</h4>
+					<section>
+						<label>
+							How many gram did the dog eat
+							<input type="text"></input>
+						</label>
+					</section>
+					<section>
+						<label>
+							When did the dog eat
+							<input type="time" defaultValue="08:00"></input>
+						</label>
+					</section>
+					<section>
+						<button onClick={handleCloseForm}>Cancel</button>
+						<button>Add</button>
+					</section>
+				</form>
+			</InputFoodContainer>
 		</>
 	);
 }
---
+++ src/components/InputFood/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+// import {pxToRem} from '../../utils/unit';
+
+const InputFoodContainer = styled.section`
+	border: 1px solid black;
+	border-radius: 10px;
+	margin: 2em;
+	padding: 0.2em 1em;
+
+	h4 {
+		display: flex;
+		justify-content: center;
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+`;
+
+export {InputFoodContainer};
