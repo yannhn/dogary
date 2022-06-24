5db06737815493eb148e02b1456bbb8538359acf
Yannik Hangst <yanso1@posteo.de>
2022-06-24 12:37:29 +0200

    add structure

--- src/components/WalkCard/index.js
+++ src/components/WalkCard/index.js
@@ -1,1 +1,30 @@
-export default function WalkCard() {}
+import {
+	FoodCardContainer,
+	FoodCardButton,
+	FoodInfoContainer,
+	FoodCardHead,
+	FoodCounter,
+	FoodInfo,
+} from './styled';
+
+export default function WalkCard() {
+	return (
+		<>
+			<FoodCardContainer>
+				<FoodCardHead>
+					<section>
+						<h2>Walks</h2>
+					</section>
+					<FoodCardButton>+</FoodCardButton>
+				</FoodCardHead>
+				<FoodInfoContainer>
+					<FoodInfo>
+						<p>Duration: 45 minutes</p>
+						<p>When: 10:45 - 11:30</p>
+					</FoodInfo>
+					<FoodCounter>45 minutes / goal</FoodCounter>
+				</FoodInfoContainer>
+			</FoodCardContainer>
+		</>
+	);
+}
