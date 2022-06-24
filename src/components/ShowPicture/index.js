4d916a1a4401798921bef6c783d54c01de1eb9a7
Yannik Hangst <100389763+yannhn@users.noreply.github.com>
2022-06-24 11:31:43 +0200

    Feature/show picture (#13)
    * add files
    
    * add mock image and import component to homepage
    
    * fix img
    
    * delete Home
    
    * fix error
    
    * add basic styling
    
    * implement feedback

---
+++ src/components/ShowPicture/index.js
@@ -0,0 +1,12 @@
+import StyledPicture from './styled';
+
+export default function ShowPicture() {
+	return (
+		<>
+			<StyledPicture
+				src="https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+				alt="mock_img"
+			></StyledPicture>
+		</>
+	);
+}
