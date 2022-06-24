d9b5921f83bed8e01a74025e3dfc14e06951ec04
Yannik Hangst <yanso1@posteo.de>
2022-06-24 12:46:18 +0200

    add storybook

---
+++ src/components/WalkCard/WalkCard.stories.js
@@ -0,0 +1,12 @@
+import WalkCard from './';
+
+export function Default() {
+	return <WalkCard>Button</WalkCard>;
+}
+
+const story = {
+	title: 'Walk Card',
+	component: WalkCard,
+};
+
+export default story;
