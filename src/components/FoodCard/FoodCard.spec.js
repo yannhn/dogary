209132be5bcbfad4de446bffcf792a944bd9d913
Yannik Hangst <yanso1@posteo.de>
2022-06-24 16:23:53 +0200

    add test for displaying card

--- src/components/FoodCard/FoodCard.spec.js
+++ src/components/FoodCard/FoodCard.spec.js
@@ -4,22 +4,34 @@ import userEvent from '@testing-library/user-event';
 
 import FoodCard from './';
 
-describe('FoodCard component', () => {
-	it('should render FoodCard component', () => {
-		const text = 'Click me';
-		render(<Button>{text}</Button>);
-		const button = screen.getByText(text);
+describe('Display FoodCard component', () => {
+	it('should render FoodCard text', () => {
+		render(<FoodCard />);
+
+		const title = screen.getByText('Food');
+		const typeOfFood = screen.getByText('Type of food');
+		const time = screen.getByText('What:', {exact: false});
+		const amount = screen.getByText('When:', {exact: false});
+		expect(title).toBeInTheDocument();
+		expect(typeOfFood).toBeInTheDocument();
+		expect(time).toBeInTheDocument();
+		expect(amount).toBeInTheDocument();
+	});
+	it('should render the FoocCard button', () => {
+		render(<FoodCard />);
+
+		const button = screen.getByRole('button');
 		expect(button).toBeInTheDocument();
 	});
 });
 
-describe('Button component', () => {
-	it('should allow clicks', async () => {
-		const handleClick = jest.fn();
-		const text = 'Click me';
-		render(<Button onClick={handleClick}>{text}</Button>);
-		const button = screen.getByText(text);
-		await userEvent.click(button);
-		expect(handleClick).toHaveBeenCalledTimes(1);
-	});
-});
+// describe('Button component', () => {
+// 	it('should allow clicks', async () => {
+// 		const handleClick = jest.fn();
+// 		const text = 'Click me';
+// 		render(<Button onClick={handleClick}>{text}</Button>);
+// 		const button = screen.getByText(text);
+// 		await userEvent.click(button);
+// 		expect(handleClick).toHaveBeenCalledTimes(1);
+// 	});
+// });
