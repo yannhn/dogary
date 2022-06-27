75b38e0536d4935de63bfe82085ffa45ef9770b6
Yannik Hangst <yanso1@posteo.de>
2022-06-27 15:20:33 +0200

    add placeholder data

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -17,12 +17,12 @@ export default function BusinessCard() {
 				</BusinessCardHead>
 				<BusinessInfoContainer>
 					<BusinessInfo>
-						<p>What: big AND/OR small</p>
+						<p>What: big AND small</p>
 						<p>When: at 07:30</p>
 					</BusinessInfo>
 					<BusinessCounter>
-						<p>0 /small</p>
-						<p>0 /big</p>
+						<p>1 /small</p>
+						<p>1 /big</p>
 					</BusinessCounter>
 				</BusinessInfoContainer>
 			</BusinessCardContainer>
