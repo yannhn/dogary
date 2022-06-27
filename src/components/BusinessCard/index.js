22de3bd15e9053ba9dcdb5b75261db8bb3b5f5a0
Yannik Hangst <yanso1@posteo.de>
2022-06-27 15:05:03 +0200

    add card structure

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -1,3 +1,23 @@
 export default function BusinessCard() {
-	return <></>;
+	return (
+		<>
+			<section>
+				<article>
+					<section>
+						<h2>"A dogs business"</h2>
+					</section>
+				</article>
+				<section>
+					<section>
+						<p>What: big AND/OR small</p>
+						<p>When: at 07:30</p>
+					</section>
+					<section>
+						<p>0 / small</p>
+						<p>0 / big</p>
+					</section>
+				</section>
+			</section>
+		</>
+	);
 }
