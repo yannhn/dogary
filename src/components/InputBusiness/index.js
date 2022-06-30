ec71687219f08de8f0ab6616c94c4930eb25cc82
Yannik Hangst <yanso1@posteo.de>
2022-06-30 15:44:04 +0200

    setup form

--- src/components/InputBusiness/index.js
+++ src/components/InputBusiness/index.js
@@ -1,6 +1,25 @@
+import {useState} from 'react';
+
 import {InputBusinessContainer} from './styled';
 
 export default function InputBusiness({}) {
+	const [smallBusiness, setSmallBusiness] = useState(false);
+	const [bigBusiness, setBigBusiness] = useState(false);
+	const [time, setTime] = useState('');
+
+	const resetForm = () => {
+		setSmallBusiness(false);
+		setBigBusiness(false);
+		setTime('');
+	};
+
+	// const handleChange = event => {
+	// 	setSmallBusiness(event.target.value);
+	// 	setBigBusiness(event.target.value);
+	// };
+
+	console.log(smallBusiness);
+
 	return (
 		<>
 			<InputBusinessContainer>
@@ -10,20 +29,36 @@ export default function InputBusiness({}) {
 						<h5>Type of business</h5>
 						<label>
 							small
-							<input type="radio"></input>
+							<input
+								type="checkbox"
+								value={smallBusiness}
+								onChange={event => setSmallBusiness(event.target.value)}
+							></input>
 						</label>
 						<label>
 							big
-							<input type="radio"></input>
+							<input
+								type="checkbox"
+								value={bigBusiness}
+								onChange={event => setBigBusiness(event.target.value)}
+							></input>
 						</label>
 					</section>
 					<section>
 						<h5>Time of business</h5>
 						<label>
-							<input type="time" defaultValue="08:00"></input>
+							<input
+								type="time"
+								defaultValue="08:00"
+								onChange={event => setTime(event.target.value)}
+							></input>
 						</label>
 					</section>
 					<button type="submit">add</button>
+					<p>
+						small - {smallBusiness}, big - {bigBusiness}, date - {time}
+					</p>
+					<p onClick={resetForm}>reset form</p>
 				</form>
 			</InputBusinessContainer>
 		</>
