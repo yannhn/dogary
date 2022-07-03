c0848ae4f6ab4d2639d713cd9c5933ac3592d490
Yannik Hangst <100389763+yannhn@users.noreply.github.com>
2022-07-03 14:26:49 +0200

    Feature/add business (#24)
    * setup file structure
    
    * import form to page
    
    * apply basic styling to form
    
    * add mock data to input
    
    * show correct value to boolean
    
    * add button to show form
    
    * setup form
    
    * add events on submit
    
    * edit button to hide form on click
    
    * apply styling to button
    
    * added ternary operator to input
    
    * implement resetHandler to update Form after submit
    
    * fix last errors
    
    * edit out style
    
    * implement feedback

---
+++ src/components/InputBusiness/index.js
@@ -0,0 +1,68 @@
+import {nanoid} from 'nanoid';
+import {useState} from 'react';
+
+import {InputBusinessContainer, InputBusinessForm} from './styled';
+
+export default function InputBusiness({addNewBusinessItem}) {
+	const [smallBusiness, setSmallBusiness] = useState(false);
+	const [bigBusiness, setBigBusiness] = useState(false);
+	const [time, setTime] = useState('08:00');
+
+	const resetHandler = () => {
+		setSmallBusiness(false);
+		setBigBusiness(false);
+		setTime(time);
+	};
+
+	const submitHandler = event => {
+		event.preventDefault();
+		const newBusinessItem = {
+			id: nanoid(),
+			smallBusiness: smallBusiness,
+			bigBusiness: bigBusiness,
+			time: time,
+		};
+		addNewBusinessItem(newBusinessItem);
+		resetHandler();
+	};
+
+	return (
+		<>
+			<InputBusinessContainer>
+				<InputBusinessForm onSubmit={submitHandler}>
+					<h2>Your dogs business</h2>
+					<section>
+						<h3>Type of business</h3>
+						<label>
+							small
+							<input
+								type="checkbox"
+								checked={smallBusiness}
+								onChange={() => setSmallBusiness(!smallBusiness)}
+							></input>
+						</label>
+						<label>
+							big
+							<input
+								type="checkbox"
+								checked={bigBusiness}
+								onChange={() => setBigBusiness(!bigBusiness)}
+							></input>
+						</label>
+					</section>
+					<section>
+						<h3>Time of business</h3>
+						<label>
+							<input
+								type="time"
+								defaultValue="08:00"
+								onChange={event => setTime(event.target.value)}
+							></input>
+						</label>
+					</section>
+					<button type="submit">add</button>
+				</InputBusinessForm>
+			</InputBusinessContainer>
+		</>
+	);
+}
