3f5e812c6a323a5e994132fe377afccf9dee748e
Yannik Hangst <100389763+yannhn@users.noreply.github.com>
2022-07-05 09:47:06 +0200

    Feature/add walks (#35)
    * add file structure
    
    * add form elements
    
    * add button to card
    
    * edit button to show form onClick
    
    * add submit to form
    
    * apply basic styling to form
    
    * edit showForm
    
    * lifted state up
    
    * edit state down
    
    * restructure functions
    
    * add map method to show inputs
    
    * apply basic styling to button
    
    * implement feedback

---
+++ src/components/InputWalk/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const InputWalkContainer = styled.section`
+	margin: 2em;
+	padding: 0.2em 1em;
+	border: 1px solid black;
+	border-radius: 10px;
+`;
+
+const InputWalkForm = styled.form`
+	text-align: center;
+`;
+
+const InputWalkButton = styled.button`
+	margin-top: 1em;
+`;
+
+export {InputWalkContainer, InputWalkForm, InputWalkButton};
