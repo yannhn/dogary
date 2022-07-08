21622e81564ab949ede878708ad071e98d604f9c
Yannik Hangst <yanso1@posteo.de>
2022-07-08 17:00:20 +0200

    apply basic styling

--- src/components/Calendar/FullCalendar/index.js
+++ src/components/Calendar/FullCalendar/index.js
@@ -2,46 +2,45 @@ import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
-import {nanoid} from 'nanoid';
+
+import {StyleWrapper} from './styled';
 
 export default function Calendar() {
-	const INITIAL_EVENTS = [
-		{
-			id: 'a',
-			title: 'go to veterinarian',
-			start: '2022-07-15',
-		},
-		{
-			id: 'b',
-			title: 'puppy school',
-			start: '2022-07-08',
-		},
-		{
-			id: 'c',
-			title: 'meeting',
-			start: '2022-07-04',
-		},
-	];
-
 	return (
 		<>
-			<FullCalendar
-				plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
-				headerToolbar={{
-					left: 'dayGridMonth,dayGridDay',
-					center: 'title',
-					right: 'prev,next today',
-				}}
-				initialView="dayGridMonth"
-				dateClick={function (info) {
-					// alert('Current view' + info.view.type);
-					// info.dayEl.style.backgroundColor = 'red';
-					// info.changeView('dayGridDay');
-					this.changeView('dayGridDay', info.dateStr);
-				}}
-				initialEvents={INITIAL_EVENTS}
-				// dateClick={e => dateClickHandler(e)}
-			/>
+			<StyleWrapper>
+				<FullCalendar
+					plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
+					firstDay={1}
+					headerToolbar={{
+						left: 'dayGridMonth',
+						center: 'title',
+						right: 'prev,next,today',
+					}}
+					initialView="dayGridMonth"
+					dateClick={function (info) {
+						this.changeView('dayGridDay', info.dateStr);
+					}}
+					events={[
+						{
+							id: 'a',
+							start: '2022-07-04',
+							display: 'background',
+						},
+
+						{
+							id: 'b',
+							start: '2022-07-08',
+							display: 'background',
+						},
+						{
+							id: 'c',
+							start: '2022-07-15',
+							display: 'background',
+						},
+					]}
+				/>
+			</StyleWrapper>
 		</>
 	);
 }
---
+++ src/components/Calendar/FullCalendar/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyleWrapper = styled.section`
+	padding: 1em;
+	.fc-bg-event {
+		background: #ea2001;
+	}
+	.fc-event-today {
+		background-color: blue;
+	}
+	.fc .fc-button-primary {
+		background-color: #1f2b4d;
+	}
+	.fc-toolbar-title {
+		font-size: 1rem;
+		text-align: center;
+	}
+`;
