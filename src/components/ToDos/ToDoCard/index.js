2fd709cffd836a802e8c6a151a8d32dc524fb073
Yannik Hangst <yanso1@posteo.de>
2022-07-11 00:23:39 +0200

    add styling to card

--- src/components/ToDos/ToDoCard/index.js
+++ src/components/ToDos/ToDoCard/index.js
@@ -2,12 +2,14 @@ import NewToDoButton from '../NewToDoButton';
 import NewToDoForm from '../NewToDoForm';
 import ToDo from '../ToDo';
 
+import {ToDoCardContainer} from './styled';
+
 export default function ToDoCard() {
 	return (
-		<>
+		<ToDoCardContainer>
 			<ToDo />
 			<NewToDoButton />
 			<NewToDoForm />
-		</>
+		</ToDoCardContainer>
 	);
 }
---
+++ src/components/ToDos/ToDoCard/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ToDoCardContainer = styled.section`
+	margin: 1em;
+	border-radius: 10px;
+	background-color: #1984c3;
+	background-image: linear-gradient(315deg, #1984c3 0%, #26a4ef 100%);
+`;
+
+export {ToDoCardContainer};
