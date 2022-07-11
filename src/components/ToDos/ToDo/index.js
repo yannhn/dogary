c7ff77ee2247237227655d6adeb393a78b9b43c4
Yannik Hangst <yanso1@posteo.de>
2022-07-11 16:37:30 +0200

    add code

--- src/components/ToDos/ToDo/index.js
+++ src/components/ToDos/ToDo/index.js
@@ -1,10 +1,31 @@
+import {useState} from 'react';
+
 import {ToDoItemContainer} from './styled';
 
-export default function ToDo({title}) {
+export default function ToDo({
+	key,
+	title,
+	setEditingText,
+	setTodoEditing,
+	editTodo,
+	editingText,
+	todoEditing,
+}) {
 	return (
 		<ToDoItemContainer>
 			<input type="checkbox"></input>
-			<p>{title}</p>
+			{todoEditing === key ? (
+				<input
+					type="text"
+					onChange={event => setEditingText(event.target.value)}
+					value={editingText}
+				/>
+			) : (
+				<p>{title}</p>
+			)}
+
+			<button onClick={setTodoEditing}>Edit ToDo</button>
+			<button onClick={editTodo}>Submit Edit</button>
 		</ToDoItemContainer>
 	);
 }
--- src/components/ToDos/ToDoList/index.js
+++ src/components/ToDos/ToDoList/index.js
@@ -9,6 +9,8 @@ import {ToDoListContainer} from './styled';
 
 export default function ToDoList() {
 	const [items, setItems] = useState([]);
+	const [todoEditing, setTodoEditing] = useState(null);
+	const [editingText, setEditingText] = useState('');
 
 	function addNewToDo(newTodo) {
 		setItems(prevItem => {
@@ -16,12 +18,32 @@ export default function ToDoList() {
 		});
 	}
 
+	function editToDo() {
+		const updatedToDos = [...items].map(item => {
+			if (key === id) {
+				item.title = editingText;
+			}
+			return item;
+		});
+		setItems(updatedToDos);
+		setTodoEditing(null);
+		setEditingText('');
+	}
+
 	return (
 		<ToDoListContainer>
 			<ToDoCardTitle />
 			<ToDoCardSection />
 			{items.map(item => (
-				<ToDo key={item.id} title={item.title} />
+				<ToDo
+					key={item.id}
+					title={item.title}
+					setEditingText={() => setEditingText(item.id)}
+					setTodoEditing={() => setTodoEditing(item.id)}
+					editTodo={() => editToDo(item.id)}
+					todoEditing={todoEditing}
+					editingText={editingText}
+				/>
 			))}
 			<NewToDoButton addNewToDo={addNewToDo} />
 		</ToDoListContainer>
