74337e15be1e7b43c3101da62091d400df2ba776
Yannik Hangst <yanso1@posteo.de>
2022-07-14 17:59:45 +0200

    insert data to new button

--- src/components/InputImage/InputImageButton/index.js
+++ src/components/InputImage/InputImageButton/index.js
@@ -1,3 +1,34 @@
+import {useState} from 'react';
+
+import FormModal from '../FormModal';
+import InputImageForm from '../InputImageForm';
+import ShowPicture from '../ShowPicture/index';
+
 export default function InputImageButton() {
-	return <></>;
+	const [showForm, setShowForm] = useState(false);
+	const [submittedItem, setSubmittedItems] = useState({image: ''});
+
+	const addNewInfo = prevItem => {
+		const newInfoItems = {...submittedItem, ...prevItem};
+		setShowForm(false);
+		setSubmittedItems(newInfoItems);
+	};
+
+	function cancelForm() {
+		setShowForm(!showForm);
+	}
+
+	return (
+		<>
+			<button onClick={() => setShowForm(!showForm)}>Show Form</button>
+			{showForm && (
+				<FormModal>
+					<InputImageForm addNewInfo={addNewInfo} cancelForm={cancelForm} />
+				</FormModal>
+			)}
+			<section>
+				<ShowPicture styledImage={submittedItem.image}></ShowPicture>
+			</section>
+		</>
+	);
 }
--- src/components/InputImage/InputImageForm/index.js
+++ src/components/InputImage/InputImageForm/index.js
@@ -1,3 +1,45 @@
-export default function InputImageForm() {
-	return <></>;
+import {nanoid} from 'nanoid';
+import {useState} from 'react';
+import {InfoImgPreview} from './styled';
+
+export default function InputImageForm({addNewInfo, cancelForm}) {
+	const [image, setImage] = useState('');
+
+	const imageChangeHandler = event => {
+		setImage(URL.createObjectURL(event.target.files[0]));
+	};
+
+	const submitHandler = event => {
+		event.preventDefault();
+		const newItems = {
+			id: nanoid(),
+
+			image: image,
+		};
+		addNewInfo(newItems);
+		setImage([]);
+	};
+
+	return (
+		<form onSubmit={submitHandler}>
+			<section>
+				<h2>Upload your image!</h2>
+				<label htmlFor="img-input">Upload profile picture: </label>
+				<input
+					id="img-input"
+					type="file"
+					accept=".png, .jpg, .jpeg"
+					onChange={imageChangeHandler}
+				></input>
+			</section>
+			<section>
+				<h2>Image Preview</h2>
+				<InfoImgPreview src={image} alt="preview" width="250" height="250" />
+			</section>
+			<button type="submit">Add</button>
+			<button type="button" onClick={cancelForm}>
+				cancel
+			</button>
+		</form>
+	);
 }
