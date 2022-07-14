a6d920450d78c500aca5712555b16b47a18951e8
Yannik Hangst <100389763+yannhn@users.noreply.github.com>
2022-07-14 13:30:53 +0200

    Feature/check todos (#61)
    * add complete function
    
    * add functionality
    
    * edit functionality
    
    * add functionality
    
    * apply basic styling
    
    * finish up styling
    
    * implement feedback
    
    * implement feedback
    
    * implement feedback

--- src/components/ToDos/ToDo/index.js
+++ src/components/ToDos/ToDo/index.js
@@ -2,14 +2,13 @@ import {useState} from 'react';
 
 import {ToDoItemContainer, ToDoItem} from './styled';
 
-export default function ToDo({id, title, editTask, deleteToDo}) {
+export default function ToDo({id, title, editTask, completeToDo, deleteToDo, completed}) {
 	const [isEditing, setIsEditing] = useState(false);
 	const [newTitle, setNewTitle] = useState('');
 
 	function handleSubmit(event) {
 		event.preventDefault();
 		editTask(id, newTitle);
-		setNewTitle('');
 		setIsEditing(false);
 	}
 
@@ -24,11 +23,18 @@ export default function ToDo({id, title, editTask, deleteToDo}) {
 						<input
 							id="new-text-input"
 							type="text"
-							value={newTitle}
+							defaultValue={title}
+							required
 							onChange={event => setNewTitle(event.target.value)}
 						/>
 						<section>
-							<button type="button" onClick={() => setIsEditing(false)}>
+							<button
+								type="button"
+								onClick={() => {
+									setIsEditing(false);
+									setNewTitle('');
+								}}
+							>
 								Cancel editing {title}
 							</button>
 							<button type="submit">Save new title for {title}</button>
@@ -37,12 +43,25 @@ export default function ToDo({id, title, editTask, deleteToDo}) {
 				</ToDoItemContainer>
 			) : (
 				<ToDoItemContainer>
-					<ToDoItem htmlFor="checkbox">
-						<input id="checkbox" type="checkbox"></input>
-						{title}
-					</ToDoItem>
-					<button onClick={() => setIsEditing(true)}>Edit</button>
-					<button onClick={deleteToDo}>Delete</button>
+					{completed ? (
+						<ToDoItem htmlFor="checkbox">
+							<input
+								id="checkbox"
+								type="checkbox"
+								defaultChecked="true"
+								onChange={completeToDo}
+							></input>
+							{title}
+							<button onClick={deleteToDo}>Delete</button>
+						</ToDoItem>
+					) : (
+						<ToDoItem htmlFor="checkbox">
+							<input id="checkbox" type="checkbox" onChange={completeToDo}></input>
+							{title}
+							<button onClick={() => setIsEditing(true)}>Edit</button>
+							<button onClick={deleteToDo}>Delete</button>
+						</ToDoItem>
+					)}
 				</ToDoItemContainer>
 			)}
 		</>
--- src/components/ToDos/ToDoList/index.js
+++ src/components/ToDos/ToDoList/index.js
@@ -3,11 +3,26 @@ import {useState} from 'react';
 import NewToDoButton from '../NewToDoButton';
 import ToDo from '../ToDo';
 import ToDoCardTitle from '../ToDoCardTitle';
+import ToDoFilterSection from '../ToDoFilterSection';
 
 import {ToDoListContainer} from './styled';
 
 export default function ToDoList() {
 	const [toDos, setToDos] = useState([]);
+	const [status, setStatus] = useState('all');
+
+	const filteredToDo = filterChangeHandler();
+
+	function filterChangeHandler() {
+		switch (status) {
+			case 'completed':
+				return toDos.filter(todo => todo.completed === true);
+			case 'uncompleted':
+				return toDos.filter(todo => todo.completed === false);
+			default:
+				return toDos;
+		}
+	}
 
 	function addNewToDo(newTodo) {
 		setToDos(prevToDo => {
@@ -24,6 +39,17 @@ export default function ToDoList() {
 		});
 		setToDos(editedToDos);
 	}
+
+	function completeToDo(id) {
+		const completedToDos = toDos.map(toDo => {
+			if (id === toDo.id) {
+				return {...toDo, completed: !toDo.completed};
+			}
+			return toDo;
+		});
+		setToDos(completedToDos);
+	}
+
 	function deleteToDo(id) {
 		const filteredToDos = toDos.filter(todo => id !== todo.id);
 		setToDos(filteredToDos);
@@ -32,12 +58,15 @@ export default function ToDoList() {
 	return (
 		<ToDoListContainer>
 			<ToDoCardTitle />
-			{toDos.map(todo => (
+			<ToDoFilterSection onChangeStatus={newStatus => setStatus(newStatus)} />
+			{filteredToDo.map(todo => (
 				<ToDo
 					key={todo.id}
 					id={todo.id}
 					title={todo.title}
+					completed={todo.completed}
 					editTask={editTask}
+					completeToDo={() => completeToDo(todo.id)}
 					deleteToDo={() => deleteToDo(todo.id)}
 				/>
 			))}
