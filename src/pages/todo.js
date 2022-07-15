20477cc92f2516e7dcf60fb50bf7f98e6c1c289b
Yannik Hangst <100389763+yannhn@users.noreply.github.com>
2022-07-15 16:48:57 +0200

    add new todo-page to navigate to (#79)

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,17 +1,17 @@
 import {Helmet} from 'react-helmet';
 
-import Calendar from '../components/Calendar/FullCalendar';
 import Layout from '../components/Layout';
+import ToDoList from '../components/ToDos/ToDoList';
 
-export default function CalendarPage() {
+export default function ToDoPage() {
 	return (
 		<Layout>
 			<Helmet>
 				<title key="title">Calendar</title>
 				<meta key="description" name="description" content="Calendar" />
 			</Helmet>
-			<h1>Calendar</h1>
-			<Calendar />
+			<h1>ToDo</h1>
+			<ToDoList />
 		</Layout>
 	);
 }
