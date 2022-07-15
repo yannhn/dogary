cd0413d7d2e5149775270708193056391054a057
Yannik Hangst <yanso1@posteo.de>
2022-07-15 14:25:26 +0200

    add new todo-page to navigate to

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import HomePage from './pages';
 import CalendarPage from './pages/calendar';
+import ToDoPage from './pages/todo';
 import {GlobalStyle} from './styles';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
 					<Route path="/">
 						<Route index element={<HomePage />} />
 						<Route path="calendar" element={<CalendarPage />} />
+						<Route path="todo" element={<ToDoPage />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
